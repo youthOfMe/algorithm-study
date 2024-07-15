b48d4cd9b17e85fd8a01752ebe01d00c93ed7edb
youthOfMe <2040690180@qq.com>
2024-07-15 22:37:32 +0800

    feat: 初始化项目, 实现十进制转二进制字符串

---
+++ src/main/java/com/yang/dachang/oneday/Code01.java
@@ -0,0 +1,19 @@
+package com.yang.dachang.oneday;
+
+public class Code01 {
+
+    public static void print(int num) {
+        String s = "";
+        for (int i = 31; i >= 0; i--) {
+            s += (num & (1 << i)) == 0 ? "0" : "1";
+        }
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        // 32位
+        int num = 3;
+
+        print(num);
+    }
+}
