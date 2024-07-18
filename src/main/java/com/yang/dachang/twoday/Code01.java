4926ed22edd9da1dcc396188d2a92dab979089c3
youthOfMe <2040690180@qq.com>
2024-07-18 23:23:29 +0800

    feat: 实现选择排序

--- src/main/java/com/yang/dachang/twoday/Code01.java
+++ src/main/java/com/yang/dachang/twoday/Code01.java
@@ -1,4 +1,28 @@
 package com.yang.dachang.twoday;
 
 public class Code01 {
+
+    public static void selectSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        final int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            int minValueIndex = i;
+            for (int j = i + 1; j < N; j++) {
+                minValueIndex = arr[j] < arr[minValueIndex] ? j  : minValueIndex;
+            }
+            swap(arr, minValueIndex, i);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/com/yang/dachang/twoday/Code02.java
+++ src/main/java/com/yang/dachang/twoday/Code02.java
@@ -1,8 +1,5 @@
 package com.yang.dachang.twoday;
 
-/**
- * 作者：洋哥
- * 描述：牛逼
- */
+
 public class Code02 {
 }
