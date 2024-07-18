1158b1a0e277a95b627bb8752ec1319c87af201c
youthOfMe <2040690180@qq.com>
2024-07-18 23:35:02 +0800

    feat: 实现插入排序

---
+++ src/main/java/com/yang/dachang/twoday/Code03.java
@@ -0,0 +1,26 @@
+package com.yang.dachang.twoday;
+
+public class Code03 {
+
+    public static void selectSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        final int N = arr.length;
+        for (int end = 1; end < N; end++) {
+            for (int pre = end - 1; pre >= 0 && arr[pre] > arr[pre + 1]; pre--) {
+                swap(arr, pre, pre + 1);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
