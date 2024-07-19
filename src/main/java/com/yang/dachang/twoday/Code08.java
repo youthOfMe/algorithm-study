dd28d1718bff53d398589e9683924e1d02069a04
youthOfMe <2040690180@qq.com>
2024-07-19 23:25:51 +0800

    feat: 实现二分查找3 -> 获取局部最小值

--- src/main/java/com/yang/dachang/twoday/Code08.java
+++ src/main/java/com/yang/dachang/twoday/Code08.java
@@ -1,4 +1,37 @@
 package com.yang.dachang.twoday;
 
+/**
+ * 找到局部最小值
+ */
 public class Code08 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getTarget(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        if (arr[left] < arr[left + 1]) {
+            return left;
+        }
+        if (arr[right] < arr[right - 1]) {
+            return right;
+        }
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] < arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+                ans = mid;
+            }
+            if (arr[mid] < arr[mid - 1]) {
+                right = mid - 1;
+                continue;
+            }
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
