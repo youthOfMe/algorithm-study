40da74a038a4ac76758bb6176e026645ac0b6225
youthOfMe <2040690180@qq.com>
2024-07-22 00:18:06 +0800

    feat: 完成逆序对问题

--- src/main/java/com/yang/dachang/timet24t7t21/Code03.java
+++ src/main/java/com/yang/dachang/timet24t7t21/Code03.java
@@ -2,9 +2,10 @@ package com.yang.dachang.timet24t7t21;
 
 import java.util.Arrays;
 
-public class Code02 {
+public class Code03 {
     public static void main(String[] args) {
-        int[] arr = {1, 5, 8, 9, 54, 456, 5, 6, 22, 35, 12, 50, 0};
+        // int[] arr = {1, 5, 8, 9, 54, 456, 5, 6, 22, 35, 12, 50, 0};
+        int[] arr = {3, 1, 0, 4, 3, 1};
         System.out.println(getTarget(arr, 0, arr.length - 1));
         System.out.println(Arrays.toString(arr));
     }
@@ -18,28 +19,28 @@ public class Code02 {
     }
 
     public static int merge(int[] arr, int left, int mid, int right) {
-        int p1 = left;
-        int p2 = mid + 1;
+        int p1 = mid;
+        int p2 = right;
         int[] help = new int[right - left + 1];
-        int i = 0;
-        int res = 0;
-        while (p1 <= mid && p2 <= right) {
-            res += arr[p1] < arr[p2] ? (right - p2 + 1) * arr[p1] : 0;
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        int i = right - left;
+        int ans = 0;
+        while (p1 >= left && p2 >= mid + 1) {
+            ans += arr[p1] > arr[p2] ? p2 - mid : 0;
+            help[i--] = arr[p1] > arr[p2] ? arr[p1--] : arr[p2--];
         }
 
-        while (p1 <= mid) {
-            help[i++] = arr[p1++];
+        while (p1 >= left) {
+            help[i--] = arr[p1--];
         }
 
-        while (p2 <= right) {
-            help[i++] = arr[p2++];
+        while (p2 >= mid + 1) {
+            help[i--] = arr[p2--];
         }
 
-        for (i = left; i <= right; i++) {
-            arr[i] = help[i - left];
+        for (i = 0; i < right - left + 1; i++) {
+            arr[left + i] = help[i];
         }
 
-        return res;
+        return ans;
     }
 }
