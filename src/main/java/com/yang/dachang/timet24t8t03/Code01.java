7781f0529f69d9df721e7fcd517d6b0b68c717aa
youthOfMe <2040690180@qq.com>
2024-08-04 08:57:04 +0800

    feat: 完成快速排序1.0版

---
+++ src/main/java/com/yang/dachang/timet24t8t03/Code01.java
@@ -0,0 +1,63 @@
+package com.yang.dachang.timet24t8t03;
+
+import java.util.Arrays;
+
+/**
+ * 快速排序1.0
+ */
+public class Code01 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 9, 5, 6, 5, 8, 2, 0, 0, 3, 25, 62, 2, 5, 5};
+        // int[] arr = {3, 1, 0, 4, 3, 1};
+        getTarget(arr, 0, arr.length -1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void getTarget(int[] arr, int left, int right) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        if (left >= right) {
+            return;
+        }
+
+        int[] quickSort = quickSort(arr, left, right);
+        getTarget(arr, left, quickSort[0]);
+        getTarget(arr, quickSort[1], right);
+    }
+
+
+
+    public static int[] quickSort(int[] arr, int left, int right) {
+        if (left > right) {
+            return new int[]{-1, -1};
+        }
+
+        if (left == right) {
+            return new int[]{left, left};
+        }
+
+        int target = right;
+        int small = left - 1;
+        int big = right;
+        int index = left;
+        while (index < big) {
+            if (arr[index] <= arr[target]) {
+                swap(arr, ++small, index++);
+            } else {
+                swap(arr, --big, index);
+            }
+        }
+        swap(arr, big++, target);
+        return new int[]{small, big};
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+}
