9779ca2fc224de2063fa04b73243b83be8a5c457
youthOfMe <2040690180@qq.com>
2024-08-07 08:23:10 +0800

    feat: 改良堆排序

--- src/main/java/com/yang/dachang/timet24t8t06/Code01.java
+++ src/main/java/com/yang/dachang/timet24t8t06/Code01.java
@@ -1,12 +1,13 @@
-package com.yang.dachang.timet24t8t05;
+package com.yang.dachang.timet24t8t06;
+
 
 import java.util.Arrays;
 
 public class Code01 {
 
     public static void main(String[] args) {
-        int[] arr = {1, 8, 9, 5, 6, 5, 8, 2, 0, 0, 3, 25, 62, 2, 5, 5};
-        // int[] arr = {3, 1, 0, 4, 3, 1};
+        // int[] arr = {1, 8, 9, 5, 6, 5, 8, 2, 0, 0, 3, 25, 62, 2, 5, 5};
+        int[] arr = {3, 1, 0, 4, 3, 1};
         getTarget(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -14,12 +15,12 @@ public class Code01 {
     public static void getTarget(int[] arr) {
         int heapSize = arr.length;
         // 整理数组为大根堆
-        for (int i = 0; i < heapSize - 1; i++) {
-            heapInsert(arr, i);
+        for (int i = heapSize - 1; i >= 0; i--) {
+            heapify(arr, i, heapSize);
         }
 
         while (heapSize > 1) {
-            swap(arr, 0, (heapSize-- - 1));
+            swap(arr, 0, --heapSize);
             heapify(arr, 0, heapSize - 1);
         }
     }
@@ -33,7 +34,7 @@ public class Code01 {
 
     public static void heapify(int[] arr, int index, int heapSize) {
         while (index * 2 + 1 < heapSize) {
-            int lastgest = index * 2 + 2 < heapSize && arr[index * 2 + 1] > arr[index * 2 + 2] ? index * 2 + 1 : index * 2 + 2;
+            int lastgest = index * 2 + 2 < heapSize && arr[index * 2 + 1] < arr[index * 2 + 2] ? index * 2 + 2 : index * 2 + 1;
             lastgest = arr[index] >= arr[lastgest] ? index : lastgest;
             if (index == lastgest) {
                 break;
