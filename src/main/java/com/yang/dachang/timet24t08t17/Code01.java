fb8cbca403b609502e1c82f352bd9326f5c1b83c
youthOfMe <2040690180@qq.com>
2024-08-17 22:51:59 +0800

    feat: 完成暴力解决最多安排会议问题

---
+++ src/main/java/com/yang/dachang/timet24t08t17/Code01.java
@@ -0,0 +1,71 @@
+package com.yang.dachang.timet24t08t17;
+
+import java.util.ArrayList;
+
+/**
+ * 贪心解决最多安排会议问题
+ */
+public class Code01 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Program> list = new ArrayList<>();
+        Program program1 = new Program(1, 5);
+        list.add(program1);
+        Program program2 = new Program(1, 2);
+        list.add(program2);
+        Program program3 = new Program(2, 8);
+        list.add(program3);
+        Program program4 = new Program(3, 6);
+        list.add(program4);
+        Program program5 = new Program(3, 11);
+        list.add(program5);
+        int result = getResult(list.toArray(new Program[list.size()]));
+        System.out.println(result);
+
+    }
+
+    public static class Program {
+        int start;
+        int end;
+
+        public Program(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static int getResult(Program[] programs) {
+        if (programs.length < 1) {
+            return 0;
+        }
+        return process(programs, 0, 0);
+    }
+
+    public static int process(Program[] programs, int done, int timeline) {
+        if (programs.length < 1) {
+            return 0;
+        }
+        int max = done;
+        for (int i = 0; i < programs.length; i++) {
+            Program curProgram = programs[i];
+            if (curProgram.start >= timeline) {
+                Program[] nextArr = removeiGetArr(programs, i);
+                max = Math.max(process(nextArr, done + 1, curProgram.end), max);
+            }
+        }
+        return max;
+    }
+
+    public static Program[] removeiGetArr(Program[] programs, int i) {
+        int addIndex = 0;
+        int N = programs.length;
+        Program[] resArr = new Program[N - 1];
+        for (int j = 0; j < N; j++) {
+            if (j != i) {
+                resArr[addIndex++] = programs[j];
+            }
+        }
+        return programs;
+    }
+}
