b0dc1f013df973ca3a97f67f414e0cbfc20b5434
youthOfMe <2040690180@qq.com>
2024-08-20 22:33:09 +0800

    feat: 力扣原题: friend-circle

--- src/main/java/com/yang/dachang/timet24t08t19/Code03.java
+++ src/main/java/com/yang/dachang/timet24t08t19/Code03.java
@@ -1,18 +1,34 @@
 package com.yang.dachang.timet24t08t19;
 
 /**
- * 数组实现并查集
+ * 力扣原题: friend-circle
  */
-public class Code02 {
+public class Code03 {
 
-    public class UNION {
+    public static void main(String[] args) {
+        System.out.println(findCircleNum(null));
+    }
+
+    public static int findCircleNum(int[][] M) {
+        M = new int[][]{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        UNION union = new UNION(M.length);
+        for (int i = 0; i < M.length; i++) {
+            for (int j = i + 1; j < M.length; j++) {
+                if (M[i][j] == 1) {
+                    union.union(i, j);
+                }
+            }
+        }
+        return union.sets;
+    }
+
+    public static class UNION {
         private int[] parents;
         private int[] sizes;
         private int[] help;
         private int sets;
 
-        private UNION(int[] dataList) {
-            final int N = dataList.length;
+        public UNION(int N) {
             sets = N;
             parents = new int[N];
             sizes = new int[N];
@@ -24,27 +40,26 @@ public class Code02 {
         }
 
         private int findHead(int data) {
-            int index = 0;
             int curData = data;
-            while (parents[data] != data) {
+            int index = 0;
+            while (parents[curData] != curData) {
                 help[index++] = curData;
                 curData = parents[curData];
             }
             for (index--; index >= 0; index--) {
                 parents[help[index]] = curData;
             }
-
             return curData;
         }
 
-        private void unoin(int a, int b) {
+        private void union(int a, int b) {
             int f1 = findHead(a);
             int f2 = findHead(b);
             if (f1 != f2) {
-                int bigData = sizes[f1] > sizes[f2] ? f1 : f2;
-                int smallData = bigData == sizes[f1] ? f2 : f1;
+                int bigData = sizes[f1] >= sizes[f2] ? f1 : f2;
+                int smallData = f1 == bigData ? f2 : f1;
                 parents[smallData] = bigData;
-                sizes[bigData] = sizes[bigData] + sizes[smallData];
+                sizes[bigData] += sizes[smallData];
                 sets--;
             }
         }
