7ef71458516cd13d43824be5eb01dc0f259e78ee
youthOfMe <2040690180@qq.com>
2024-09-03 18:02:34 +0800

    feat: 完成打印最宽层的二叉树节点数量

--- src/main/java/com/yang/dachang/timet24t09/time03/Code01.java
+++ src/main/java/com/yang/dachang/timet24t09/time03/Code01.java
@@ -1,6 +1,8 @@
 package com.yang.dachang.timet24t09.time03;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * 打印最宽层的二叉树节点数量
@@ -8,13 +10,27 @@ import java.util.LinkedList;
 public class Code01 {
 
     public static void main(String[] args) {
-
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        treeNode.left = treeNode2;
+        treeNode.right = treeNode3;
+        TreeNode treeNode4 = new TreeNode(4);
+        treeNode3.right = treeNode4;
+        // System.out.println(getTarget(treeNode));
+        System.out.println(getTargetMore(treeNode));
     }
 
     public static class TreeNode {
         public int value;
         public TreeNode left;
         public TreeNode right;
+
+        public TreeNode() {};
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
     }
 
     public static int getTarget(TreeNode treeNode) {
@@ -38,6 +54,42 @@ public class Code01 {
             if (curNode == end) {
                 end = nextEnd;
                 res = Math.max(res, curRes);
+                curRes = 0;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 使用更多数据结构 -> 开辟一个HashMap
+     * 使用HashMap记录每个元素
+     *
+     * @param treeNode
+     * @return
+     */
+    public static int getTargetMore(TreeNode treeNode) {
+        int res = 0;
+        int curRes = 0;
+        int curLevel = 1;
+        Map<TreeNode, Integer> nodeLevelMap = new HashMap<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(treeNode);
+        nodeLevelMap.put(treeNode, 1);
+        while (!queue.isEmpty()) {
+            TreeNode curNode = queue.pop();
+            int curNodeLevel = nodeLevelMap.get(curNode);
+            if (curNodeLevel != curLevel) {
+                curLevel += 1;
+                res = Math.max(res, curRes);
+            }
+            if (curNode.left != null) {
+                nodeLevelMap.put(curNode.left, curLevel + 1);
+            }
+            if (curNode.right != null) {
+                nodeLevelMap.put(curNode.right, curLevel + 1);
+            }
+            if (curNodeLevel == curLevel) {
+                curRes++;
             }
         }
         return res;
