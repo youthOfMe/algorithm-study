889604ceee4df7b77a9dd40365374ca1f65b0d8a
youthOfMe <2040690180@qq.com>
2024-09-05 18:03:25 +0800

    feat: 暴力递归: 完成不开辟任何新数据结构翻转栈

---
+++ src/main/java/com/yang/dachang/timet24t09/time04/Code01.java
@@ -0,0 +1,42 @@
+package com.yang.dachang.timet24t09.time04;
+
+import java.util.Stack;
+
+/**
+ * 不申请任何新的数据结构，逆序一个栈
+ */
+public class Code01 {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        reverse(stack);
+        for (Integer i : stack) {
+            System.out.println(i);
+        }
+    }
+
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+
+        int i = f(stack);
+        reverse(stack);
+        stack.push(i);
+    }
+
+    public static int f(Stack<Integer> stack) {
+        int result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        } else {
+            int last = f(stack);
+            stack.push(result);
+            return last;
+        }
+    }
+
+}
