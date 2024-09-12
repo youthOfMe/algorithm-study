00ac9e084a0ae15ad0534b0d01f9f2503b3cd759
youthOfMe <2040690180@qq.com>
2024-09-12 14:11:03 +0800

    feat: 完成剪纸问题-暴力递归

---
+++ src/main/java/com/yang/dachang/timet24t09/time11/Code02.java
@@ -0,0 +1,49 @@
+package com.yang.dachang.timet24t09.time11;
+
+/**
+ * 剪纸问题
+ */
+public class Code02 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getTarget(String target, String[] strings) {
+
+    }
+
+    public static int process(String target, String[] strings) {
+        if (target.isEmpty()) return 0;
+        int min = Integer.MAX_VALUE;
+        for (String string : strings) {
+            String minus = minus(target, string);
+            if (minus.length() == target.length()) {
+                min = Math.min(min, process(target, strings));
+            }
+        }
+
+
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
+    }
+
+    public static String minus(String target, String item) {
+        char[] tarArr = target.toCharArray();
+        char[] itemArr = item.toCharArray();
+        int[] count = new int[26];
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : tarArr) {
+            count[c - 'a']++;
+        }
+        for (char c : itemArr) {
+            count[c - 'a']--;
+        }
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 0) {
+                stringBuilder.append(i + 'a');
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
