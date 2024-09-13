a88d09cb3dfb329e49104699234d6aaa3d58cd1a
youthOfMe <2040690180@qq.com>
2024-09-13 12:42:47 +0800

    feat: 完成数字字符串转换问题-动态规划

---
+++ src/main/java/com/yang/dachang/timet24t09/time12/Code02.java
@@ -0,0 +1,34 @@
+package com.yang.dachang.timet24t09.time12;
+
+/**
+ * 数字字符串转换问题
+ */
+public class Code02 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getTarget(String target) {
+        if (target == null || target.isEmpty()) return 0;
+        final int N = target.length();
+        int[] ints = new int[N + 2];
+        char[] chars = target.toCharArray();
+        int ways = 0;
+        ints[N] = 1;
+        for (int i = N - 1; i >= 0; i--) {
+            if (chars[i] != '0') {
+                ways += ints[i + 1];
+                if ((chars[i] - '0') * 10 + chars[i + 1] - '0' < 27) {
+                    ways += ints[i + 2];
+                }
+            }
+        }
+        return ways;
+    }
+
+    public static int process(char[] tarArr, int index) {
+
+    }
+
+}
