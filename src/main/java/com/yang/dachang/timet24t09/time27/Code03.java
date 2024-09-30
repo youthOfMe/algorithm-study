6cd098717f7981e32ada46392217099cba5f6589
youthOfMe <2040690180@qq.com>
2024-09-30 12:10:24 +0800

    feat: 滑动窗口第四题，货币数组问题，从左向右的动态规划

--- src/main/java/com/yang/dachang/timet24t09/time27/Code03.java
+++ src/main/java/com/yang/dachang/timet24t09/time27/Code03.java
@@ -7,7 +7,8 @@ public class Code03 {
 
     public static void main(String[] args) {
         int[] arr = new int[]{3, 1, 3, 5, 1, 1, 1, 5, 3, 2};
-        System.out.println(getTarget(arr, 15));
+        System.out.println(getTarget(arr, 15) == dp(arr, 15));
+        System.out.println(dp(arr, 15));
     }
 
     public static int getTarget(int[] arr, int aim) {
@@ -37,6 +38,29 @@ public class Code03 {
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
+    public static int dp(int[] arr, int aim) {
+        final int N = arr.length;
+        int[][] dp = new int[N + 1][aim + 1];
+        for (int index = 0; index <= N; index++) {
+            for (int rest = 0; rest <= aim; rest++) {
+                dp[index][rest] = -1;
+            }
+        }
+        dp[N][0] = 0;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                int min = Integer.MAX_VALUE;
+                if (dp[index + 1][rest] != -1) {
+                    min = Math.min(min, dp[index + 1][rest]);
+                }
+                if (rest - arr[index] >= 0 && dp[index + 1][rest - arr[index]] != -1) {
+                    min = Math.min(min, dp[index + 1][rest - arr[index]] + 1);
+                }
+                dp[index][rest] = min == Integer.MAX_VALUE ? -1 : min;
+            }
+        }
+        return dp[0][aim];
+    }
 
 
 }
